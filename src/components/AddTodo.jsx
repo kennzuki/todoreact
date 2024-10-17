2311cea306616ad815a4de1f23ab7e465037174e
kennzuki <kingsyken@gmail.com>
2024-10-17 23:12:11 +0300

    priority and add todo state

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
+
 const AddTodo = () => {
+
+  const [addTodos, setAddTodos] = useState('')
+  const [priority,setPriority]=useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+}
+
   return (
-    <div className=' mt-6 flex place-items-center justify-evenly gap-4'>
+    <div className=' p-8'>
+      <form onSubmit={handleSubmit} className="mt-6 flex place-items-center justify-evenly gap-4">
       <input
         type='text'
         className='border w-full p-4 rounded-xl m-12'
         placeholder='Add'
-      />
+        value={addTodos}
+        onChange={(e)=>setAddTodos(e.target.value)}
+        />
+        <select
+          placeholder='priority'
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          className="px-4 py-4 border rounded-lg bg-blue-500 text-white"
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
       <button className='py-2 px-3 w-1/2  bg-blue-400 text-white font-bold rounded-xl'>
         Add
       </button>
+      </form>
+     
     </div>
   );
 };
